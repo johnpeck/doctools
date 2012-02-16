b7f3763bb262acb063a39c93e53078a1d927e147
John Peck <sunnyvalejp@gmail.com>
2012-02-15 16:24:28 -0800

    Updated startxfig script to be ok with spaces in pwd.

--- bin/startxfig.sh
+++ bin/startxfig.sh
@@ -4,8 +4,11 @@
 # Starts xfig with options
 #
 # Usage: startxfig.sh
-libstr="$PWD/doctools/xfig"
-xfig -library_dir $libstr \
+
+thisdir="$PWD"
+xfigdir="/doctools/xfig"
+libstr=$thisdir$docdir
+xfig -library_dir "$libstr" \
 	-cbg beige -freehand_resolution 1 \
 	-nosplash -showlengths -startpsFont Helvetica \
 	-inches

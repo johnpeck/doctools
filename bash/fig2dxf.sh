ae7ca9f741591dbc1a35563ec0df2e18cdb4a3b4
John Peck <john@hippo.(none)>
2012-05-04 16:15:51 -0700

    Added fig2dxf for converting a fig file to dxf.

--- bash/fig2dxf.sh
+++ bash/fig2dxf.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
-# Name: fig2png.sh
+# Name: fig2dxf.sh
 #
-# Converts a fig file generated with xfig to a png using ghostscript
-# and imagemagick
-#
-# Usage: fig2png.sh <fig file to open>
+# Converts a fig file generated with xfig to a dxf using pstoedit
+# 
+# Usage: fig2dxf.sh <fig file to open>
 
 set -e # bash should exit the script if any statement returns a non-true 
        #return value
 if [ -z "$1" ]
 then
-    echo "Usage: fig2png.sh <fig file>"
+    echo "Usage: fig2dxf.sh <fig file>"
 else  
     # Create some temporary files with the same base name but
     # different file extensions.  Always quote filename parameters to
     # preserve space characters.
     epsfile="$(echo $1|sed 's/\..\{3\}$/.eps/')"
-    pngfile="$(echo $1|sed 's/\..\{3\}$/.png/')"
+    dxffile="$(echo $1|sed 's/\..\{3\}$/.dxf/')"
     # Use transfig to create the Encapsulated Postscript (eps) file.
     # Options:
     # -L -- language.  Use eps for Encapsulated Postscript
+    # -m -- magnification.  Set this to 1 for original size.  This can't
+    #       be used with the -Z option.
     # -Z -- maxdimension.  Chooses magnification based on a maximum dimension.
     #       The units are inches if the fig file was created with imperial
     #       units, and centimeters otherwise.
-    fig2dev -L eps -Z 8 "$1" "$epsfile"
-    # Convert to png using ghostscript.
-    gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -dGraphicsAlphaBits=4 \
-        -sOutputFile="$pngfile" -r300 "$epsfile"
-    # Finally, use Imagemagick to trim the image.  Note that all resizing
-    # should be done in the fig2dev step to preserve image resolution.
-    convert "$pngfile" -trim +repage "$pngfile"
+    fig2dev -L eps -m 1 "$1" "$epsfile"
+    # Convert to dxf using pstoedit.
+    pstoedit -f "dxf" "$epsfile" "$dxffile"
 fi

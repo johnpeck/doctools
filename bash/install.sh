8a0d44f7fc0e745ebc0063d999c47070676f317e
John Peck <john@hippo.(none)>
2012-05-08 15:13:54 -0700

    Install.sh script now copies xfig contents -- not xfig directory.

--- bash/install.sh
+++ bash/install.sh
@@ -7,15 +7,24 @@
 set -e # bash should exit the script if any statement returns a non-true 
        #return value
 thisdir="$PWD"
-xfigdir="$PWD/../xfig"
+xfigdir="$PWD/../xfig" # Where my xfig figures are sourced from
+xfigdest="/usr/local/share/docxfig" # Where the figures will be copied to
+
+if [ ! -d "$xfigdest" ]; then
+    	mkdir "$xfigdest"
+	echo "Created xfig figure library at" "$xfigdest"
+fi
+
+if [ -d "$xfigdest" ]; then
+	cp --recursive "$xfigdir/." "$xfigdest"
+	echo "Copied xfig figure libraries to" "$xfigdest"
+fi
 cp txt2pdf.sh "/usr/local/bin"
 echo "Copied txt2pdf.sh to /usr/local/bin."
 cp pdfpage.sh "/usr/local/bin"
 echo "Copied pdfpage.sh to /usr/local/bin."
 cp fig2dxf.sh "/usr/local/bin"
 echo "Copied fig2dxf.sh to /usr/local/bin."
-cp --recursive "$xfigdir" "/usr/local/share/docxfig"
-echo "Copied xfig figure libraries to /usr/local/share/docxfig"
 cp startxfig.sh "/usr/local/bin"
 echo "Copied startxfig.sh to /usr/local/bin."
 

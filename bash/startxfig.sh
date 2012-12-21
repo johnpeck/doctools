e19b36f7d87a303c3c1901c91864e39cc30a3bb0
John Peck <sunnyvalejp@gmail.com>
2012-12-21 15:31:14 -0800

    Changed case of variable in bash script

--- bash/startxfig.sh
+++ bash/startxfig.sh
@@ -4,18 +4,19 @@
 # Starts xfig with options
 #
 # Usage: startxfig.sh <fig file to open>
-thisdir="$PWD"
+THISDIR="$(pwd -P)"
+
 # Location of my personal xfig libraries is symbolically linked to
 # /usr/local/share/xfig after running the install script in bash.
-libstr="/usr/local/share/docxfig"
+LIBSTR="/usr/local/share/docxfig"
 if [ -z "$1" ] 
 then
-	xfig -library_dir "$libstr" \
+	xfig -library_dir "$LIBSTR" \
 		-cbg beige -freehand_resolution 1 \
 		-nosplash -showlengths -startpsFont Helvetica \
 		-inches
 else
-	xfig -library_dir "$libstr" \
+	xfig -library_dir "$LIBSTR" \
 		-cbg beige -freehand_resolution 1 \
 		-nosplash -showlengths -startpsFont Helvetica \
 		-inches "$1"

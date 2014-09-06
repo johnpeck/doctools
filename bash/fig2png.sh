9470e54c01951d181c1a6a13c6422a30b6483d3d
John Peck <sunnyvalejp@gmail.com>
2014-09-06 08:17:44 -0500

    Changed fig2png to accept pixel dimensions

--- bash/fig2png.sh
+++ bash/fig2png.sh
@@ -4,13 +4,13 @@
 # Converts a fig file generated with xfig to a png using ghostscript
 # and imagemagick
 #
-# Usage: fig2png.sh <max dimension> <fig file to open>
+# Usage: fig2png.sh <max pixels> <fig file to open>
 
 set -e # bash should exit the script if any statement returns a
        # non-true return value
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: fig2png.sh <max dimension> <fig file>"
+    echo "Usage: fig2png.sh <max pixels> <fig file>"
     exit 64
 fi
  
@@ -25,7 +25,8 @@ pngfile="$(echo $2|sed 's/\..\{3\}$/.png/')"
 # -Z -- maxdimension.  Chooses magnification based on a maximum dimension.
 #       The units are inches if the fig file was created with imperial
 #       units, and centimeters otherwise.  Assumes 300dpi.
-fig2dev -L eps -Z "$1" "$2" "$epsfile"
+zsize=$(echo "scale=2; $1 / 300" | bc) 
+fig2dev -L eps -Z $zsize "$2" "$epsfile"
 # Convert to png using ghostscript.
 gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -dGraphicsAlphaBits=4 \
     -sOutputFile="$pngfile" -r300 "$epsfile"
